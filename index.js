1c1287221d167249e20b83eadcfc62c2ae41ffe9
Stavros Schizas <stavros.schizas@gmail.com>
2017-04-22 13:34:54 +0300

    Added index.js
    Signed-off-by: Stavros Schizas <stavros.schizas@gmail.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+/*!
+ * msgpack-response
+ * Copyright(c) 2017 Mobiltron Inc
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+var msgpackLite = require('msgpack-lite')
+
+
+module.exports = function(options) {
+  return function(req, res, next) {
+
+    // Implement the middleware function based on the options object
+
+
+    next()
+  }
+}
