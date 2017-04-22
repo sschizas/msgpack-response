51192a7bf004345e978870fc21ef13ad6ceef3be
Stavros Schizas <stavros.schizas@gmail.com>
2017-04-22 22:24:21 +0300

    Added check for all requirements
    Signed-off-by: Stavros Schizas <stavros.schizas@gmail.com>

--- index.js
+++ index.js
@@ -4,20 +4,36 @@
  * MIT Licensed
  */
 
-'use strict'
 
 /**
  * Module dependencies.
  */
-var msgpackLite = require('msgpack-lite')
+var _ = require('lodash');
+var typeis = require('type-is');
+var msgpackLite = require('msgpack-lite');
 
 
 module.exports = function(options) {
   return function(req, res, next) {
 
-    // Implement the middleware function based on the options object
+    // Check if we have all requirements in place.
+    if (typeis(res, ['application/json']) === false || !typeis.hasBody(res) || isMgsPackSupported(req) === false) {
+      return next()
+    }
+    
+    var accept = req.headers['accept-encoding']
+      , write = res.write
+      , end = res.end
+      , compress = true
+      , stream;
 
+    // Implement the middleware function
 
     next()
-  }
+  };
+};
+
+
+function isMgsPackSupported(req) {
+  return _.isNil(req.headers['accept']) === false && req.headers['accept'] === 'application/x-msgpack'
 }
