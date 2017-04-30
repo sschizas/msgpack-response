acd370fbeb606107b28925262efb9b4761cfdc38
Stavros Schizas <stavros.schizas@gmail.com>
2017-05-01 00:15:37 +0300

    Wrapping all this together
    Signed-off-by: Stavros Schizas <stavros.schizas@gmail.com>

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
  * Module dependencies.
  */
 const _ = require('lodash');
-const typeis = require('type-is');
 const mung = require('express-mung');
 const msgpackLite = require('msgpack-lite');
 
@@ -28,17 +27,27 @@ var cacheControlNoTransformRegExp = /(?:^|,)\s*?no-transform\s*?(?:,|$)/;
  */
 function msgpack (body, req, res) {
   //Check for cache-control.
-  if (shouldTransform(req, res) === false) {
+  if (shouldTransform(res) === false) {
     return body;
   }
 
   //Check if msgpack is supported by client.
-  if (isMgsPackSupported(req, res) === false) {
+  if (isMgsPackSupported(req) === false) {
     return body;
   }
 
-  //Check if body is JSON otherwise don't bother to msgpack it.
+  //Check if it is JSON otherwise don't bother to msgpack it.
+  if (res.headers['Content-Type'] !== 'application/json') {
+    return body;
+  }
 
+  //Msgpack body
+  var encodedBody = msgpackLite.encode(body);
+
+  // header fields
+  res.removeHeader('Content-Length');
+  res.set('Content-Type', 'application/x-msgpack');
+  return encodedBody;
 }
 
 
@@ -46,8 +55,8 @@ function msgpack (body, req, res) {
  * Determine if the request supports msgpack.
  * @private
  */
-function isMgsPackSupported (req, res) {
-  return _.isNil(req.headers['accept']) === false && req.headers['accept'] === 'application/x-msgpack'
+function isMgsPackSupported (req) {
+  return _.isNil(req.headers['Accept']) === false && req.headers['Accept'] === 'application/x-msgpack'
 }
 
 
@@ -55,7 +64,7 @@ function isMgsPackSupported (req, res) {
  * Determine if the entity should be transformed.
  * @private
  */
-function shouldTransform (req, res) {
+function shouldTransform (res) {
   var cacheControl = res.headers['Cache-Control'];
 
   // Don't compress for Cache-Control: no-transform
